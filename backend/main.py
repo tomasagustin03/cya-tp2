11eafd522f7ddef79bf3310a22e5f44565d98824
Franco Nicolas Clavijo <cclavijo@fi.uba.ar>
2024-07-02 18:00:37 -0300

    correccion tablas

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, jsonify
-from models import db, Carpincho, Ayudante, Asado, Compra, Nivel
+from models import db, Carpincho, Ayudante, Mostrador, Nivel
 
 app = Flask(__name__)
 port = 5000
 app.config['SQLALCHEMY_DATABASE_URI']='postgresql+psycopg2://esobrad:esobrad@localhost:5432/cya'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=True
+ayudante =Ayudante(id_ayudante=1, nombre='Peron', costo=1000, bonificacion=50, obtenido=False)
+
 
 @app.route('/')
 def hello_world():
@@ -14,6 +16,7 @@ if __name__ == '__main__':
     print('starting server...')
     db.init_app(app)
     with app.app_context():
+        #db.drop_all()
         db.create_all()
     print('started...')
     app.run(host='0.0.0.0', debug=True, port=port)

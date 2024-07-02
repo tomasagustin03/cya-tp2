c2116874ba086ce35ed6cd106b5267c4274fbcc7
Franco Nicolas Clavijo <cclavijo@fi.uba.ar>
2024-07-02 11:16:58 -0300

    correccion de tablas, start up del main, creacion de carpincho, flask run

--- backend/main.py
+++ backend/main.py
@@ -3,4 +3,17 @@ from models import db, Carpincho, Ayudante, Asado, Compra, Nivel
 
 app = Flask(__name__)
 port = 5000
-app.config('SQLALCHEMY_DATABASE_URI') 
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql+psycopg2://esobrad:esobrad@localhost:5432/cya'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+
+@app.route('/')
+def hello_world():
+    return 'esta es mi pagina'
+
+if __name__ == '__main__':
+    print('starting server...')
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
+    print('started...')
+    app.run(host='0.0.0.0', debug=True, port=port)
--- backend/models.py
+++ backend/models.py
@@ -3,34 +3,35 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class Carpincho(db.model):
-    _tablename_ = 'carpinchos'
+class Carpincho(db.Model):
+    __tablename__ = 'carpinchos'
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(255), nullable=False)
     nivel = db.Column(db.Integer, nullable=False)
     tiempo_de_coccion = db.Column(db.Integer, nullable=False)
 
-class Ayudante(db.model):
-    _tablename_ = 'tipos_de_ayudantes'
-    id = db.Column(db.Integer, primary_key=True)
+class Ayudante(db.Model):
+    __tablename__ = 'ayudantes'
+    id_ayudante = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(255), nullable=False)
     costo = db.Column(db.Integer, nullable=False)
     bonificacion = db.Column(db.Integer, nullable=False)
 
-class Asado(db.model):
-    _tablename_ = 'venta_de_asados'
-    id = db.Column(db.Integer, primary_key=True)
-    id_carpincho = db.Column(db.Integer, db.ForeignKey('carpinchos'))
+class Asado(db.Model):
+    __tablename__ = 'venta_de_asados'
+    id_asado = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, db.ForeignKey('carpinchos'))
     ganancia = db.Column(db.Integer, nullable=False)
-    fecha_de_venta = db.Column(db.datetime, default = datetime.datetime.now())
+    fecha_de_venta = db.Column(db.DateTime, default = datetime.datetime.now())
 
-class Compra(db.model):
-    _tablename_ = 'compra_de_personajes'
-    id = db.Column(db.Integer, primary_key=True)
-    id_carpincho = db.Column(db.Integer, db.ForeignKey('carpinchos'))
+class Compra(db.Model):
+    __tablename__ = 'compra_de_personajes'
+    id_compra = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, db.ForeignKey('carpinchos'))
     id_ayudante = db.Column(db.Integer, db.ForeignKey('ayudantes'))
     costo = db.Column(db.Integer, nullable=False)
 
-class Nivel(db.mo0del):
-    id = db.Column(db.Integer, primary_key=True)
-    id_carpincho = db.Column(db.Integer, db.ForeignKey('carpinchos'))
+class Nivel(db.Model):
+    __tablename__ = 'nivel'
+    id_nivel = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, db.ForeignKey('carpinchos'))

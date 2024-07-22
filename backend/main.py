0193b9ca241d222bdbd14257b660e19621a3d52f
den <tcari@fi.uba.ar>
2024-07-22 15:10:05 -0300

    limpieza

--- backend/main.py
+++ backend/main.py
@@ -9,12 +9,6 @@ CORS(app)
 port = 5000
 app.config['SQLALCHEMY_DATABASE_URI']='postgresql+psycopg2://esobrad:esobrad@localhost:5432/cya'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=True
-ayudante =Ayudante(id_ayudante=1, nombre='Peron', costo=1000, bonificacion=50, obtenido=False)
-
-
-@app.route('/', methods=["GET"])
-def hello_world():
-    return 'index.html'
 
 @app.route("/carpincho", methods=["GET"])
 def carpincho():
@@ -28,7 +22,7 @@ def carpincho():
         }
         return jsonify(carpincho_data)
     except:
-        return jsonify({'mensaje': 'El carpincho no existe'})
+        return jsonify({'mensaje': 'El carpincho no existe'}), 404
 
 @app.route('/carpincho', methods=["POST"])
 def nuevo_carpincho():
@@ -47,19 +41,22 @@ def nuevo_carpincho():
     
 @app.route('/mostrador', methods=["GET"])
 def obtener_mostrador():
-    mostrador = Mostrador.query.order_by(Mostrador.id_asado).all()
-    print(mostrador)
-    ayudante = Ayudante.query.order_by(Ayudante.id_ayudante).all()
-    print(ayudante)
-    mostrador_data = []
-    for asado in mostrador:
-        imagen_url = ayudante[asado.id_ayudante - 1].imagen_url
-        mostrador_data.append({
-            'imagen_url': imagen_url,
-            'platos': asado.platos
-        })
-    print(mostrador_data)
-    return jsonify(mostrador_data)
+    try:
+        mostrador = Mostrador.query.order_by(Mostrador.id_asado).all()
+        print(mostrador)
+        ayudante = Ayudante.query.order_by(Ayudante.id_ayudante).all()
+        print(ayudante)
+        mostrador_data = []
+        for asado in mostrador:
+            imagen_url = ayudante[asado.id_ayudante - 1].imagen_url
+            mostrador_data.append({
+                'imagen_url': imagen_url,
+                'platos': asado.platos
+            })
+        print(mostrador_data)
+        return jsonify(mostrador_data)
+    except Exception as e:
+        return jsonify({'message': 'Error al obtener el mostrador', 'error': str(e)}), 404
 
 @app.route('/mostrador', methods=["POST"])
 def agregar_asado():
@@ -89,40 +86,49 @@ def eliminar_asado():
     
 @app.route('/imagen', methods=['GET'])
 def get_images():
-    id_ayudante = Ayudante.query.where(Ayudante.obtenido == True).order_by(desc(Ayudante.id_ayudante)).first().id_ayudante
-    imagen = Ayudante.query.get(id_ayudante).imagen_url
-    print(imagen)
-    return jsonify(imagen)
+    try:
+        id_ayudante = Ayudante.query.where(Ayudante.obtenido == True).order_by(desc(Ayudante.id_ayudante)).first().id_ayudante
+        imagen = Ayudante.query.get(id_ayudante).imagen_url
+        print(imagen)
+        return jsonify(imagen)
+    except Exception as e:
+        return jsonify({'message': 'Error al obtener la imagen', 'error': str(e)}), 404
 
 @app.route('/plata', methods=['GET'])
 def get_plata():
-    carpincho = Carpincho.query.get(1)
-    plata = carpincho.plata
-    return jsonify(plata)
+    try:
+        carpincho = Carpincho.query.get(1)
+        plata = carpincho.plata
+        return jsonify(plata)
+    except Exception as e:
+        return jsonify({'message': 'Error al obtener la plata', 'error': str(e)}), 404
 
 @app.route('/niveles', methods=['GET'])
 def get_niveles():
-    niveles = Nivel.query.order_by(Nivel.id_nivel).all()
-    niveles_data = []
-    estado = 2
-    for nivel in niveles:
-        if (nivel.obtenido):
-            print(niveles_data) 
-            niveles_data.append({
-                'id_nivel': nivel.id_nivel,
-                'estado': 3,
-                'costo': nivel.costo,
-                'tiempo_de_coccion': nivel.tiempo_de_coccion
-            })
-        else:
-            niveles_data.append({
-                'id_nivel': nivel.id_nivel,
-                'estado': estado,
-                'costo': nivel.costo,
-                'tiempo_de_coccion': nivel.tiempo_de_coccion
-            })
-            estado = 1
-    return jsonify(niveles_data)
+    try:
+        niveles = Nivel.query.order_by(Nivel.id_nivel).all()
+        niveles_data = []
+        estado = 2
+        for nivel in niveles:
+            if (nivel.obtenido):
+                print(niveles_data) 
+                niveles_data.append({
+                    'id_nivel': nivel.id_nivel,
+                    'estado': 3,
+                    'costo': nivel.costo,
+                    'tiempo_de_coccion': nivel.tiempo_de_coccion
+                })
+            else:
+                niveles_data.append({
+                    'id_nivel': nivel.id_nivel,
+                    'estado': estado,
+                    'costo': nivel.costo,
+                    'tiempo_de_coccion': nivel.tiempo_de_coccion
+                })
+                estado = 1
+        return jsonify(niveles_data)
+    except Exception as e:
+        return jsonify({'message': 'Error al obtener los niveles', 'error': str(e)}), 404
 
 @app.route('/ultimonivel', methods=['GET'])
 def ultimo_nivel():
@@ -133,29 +139,32 @@ def ultimo_nivel():
         else:
             return jsonify({'mensaje': 'Exito', 'id_ultimo_nivel': ultimo_nivel.id_nivel, 'costo': ultimo_nivel.costo, 'tiempo_de_coccion': ultimo_nivel.tiempo_de_coccion})
     except Exception as e:
-        return jsonify({'mensaje': 'Error', 'id_ultimo_nivel': 'Null', 'costo': 'Null', 'tiempo_de_coccion': 'Null'})
+        return jsonify({'message': 'Error al obtener el ultimo nivel', 'error': str(e)}), 404
 
 @app.route('/reiniciar', methods=['PUT'])
 def reiniciarprogreso():
-    carpincho = Carpincho.query.get(1)
-    print(carpincho)
-    carpincho.plata = 0
-    carpincho.nombre = 'Asador'
+    try:
+        carpincho = Carpincho.query.get(1)
+        print(carpincho)
+        carpincho.plata = 0
+        carpincho.nombre = 'Asador'
 
-    ayudantes = Ayudante.query.where(Ayudante.id_ayudante > 1)
-    for ayudante in ayudantes:
-        ayudante.obtenido = False
+        ayudantes = Ayudante.query.where(Ayudante.id_ayudante > 1)
+        for ayudante in ayudantes:
+            ayudante.obtenido = False
 
-    mostrador = Mostrador.query.all()
-    for plato in mostrador:
-        plato.platos = 0
+        mostrador = Mostrador.query.all()
+        for plato in mostrador:
+            plato.platos = 0
 
-    niveles = Nivel.query.where(Nivel.id_nivel > 1)
-    for nivel in niveles:
-        nivel.obtenido = False
+        niveles = Nivel.query.where(Nivel.id_nivel > 1)
+        for nivel in niveles:
+            nivel.obtenido = False
 
-    db.session.commit()
-    return jsonify({})
+        db.session.commit()
+        return jsonify({'message' : 'Progreso reiniciado'})
+    except Exception as e:
+        return jsonify({'message': 'Error al reiniciar el progreso', 'error': str(e)}), 500
  
 @app.route('/plata', methods=['PUT'])
 def put_plata():
@@ -174,24 +183,27 @@ def put_plata():
     
 @app.route('/islas', methods=['GET'])
 def get_islas():
-    ayudantes = Ayudante.query.order_by(Ayudante.id_ayudante).all()
-    islas_data = []
-    estado = 2
-    for ayudante in ayudantes:
-        if (ayudante.obtenido):
-            islas_data.append({
-                'id_ayudante': ayudante.id_ayudante,
-                'estado': 3,
-                'costo': ayudante.costo
-            })
-        else:
-            islas_data.append({
-                'id_ayudante': ayudante.id_ayudante,
-                'estado': estado,
-                'costo': ayudante.costo
-            })
-            estado = 1
-    return jsonify(islas_data)
+    try:
+        ayudantes = Ayudante.query.order_by(Ayudante.id_ayudante).all()
+        islas_data = []
+        estado = 2
+        for ayudante in ayudantes:
+            if (ayudante.obtenido):
+                islas_data.append({
+                    'id_ayudante': ayudante.id_ayudante,
+                    'estado': 3,
+                    'costo': ayudante.costo
+                })
+            else:
+                islas_data.append({
+                    'id_ayudante': ayudante.id_ayudante,
+                    'estado': estado,
+                    'costo': ayudante.costo
+                })
+                estado = 1
+        return jsonify(islas_data)
+    except Exception as e:
+        return jsonify({'message': 'Error al obtener las islas', 'error': str(e)}), 404
 
 @app.route('/comprarayudante', methods=["POST"])
 def comprar_ayudante():
@@ -205,7 +217,7 @@ def comprar_ayudante():
         db.session.commit()
         return jsonify({'message': 'Ayudante comprado', 'ayudante': {'id': ayudante.id_ayudante, 'nombre': ayudante.nombre}, 'exitoso': 'true'})
     except Exception as e:
-        return jsonify({'message': 'Error al comprar el ayudante', 'error': str(e), 'exitoso': 'false'})
+        return jsonify({'message': 'Error al comprar el ayudante', 'error': str(e), 'exitoso': 'false'}), 500
     
 @app.route('/carpincho/nivel/<id_nivel>', methods=["POST"])
 def comprar_nivel(id_nivel):
@@ -221,7 +233,7 @@ def comprar_nivel(id_nivel):
         print(f"Nivel comprado: {nivel}")
         return jsonify({'exito': True, 'nivel': {'id': nivel.id_nivel, 'obtenido': nivel.obtenido}})
     except Exception as e:
-        return jsonify({'exito': False, 'error': str(e)})
+        return jsonify({'exito': False, 'error': str(e)}), 500
 
 if __name__ == '__main__':
     print('starting server...')
